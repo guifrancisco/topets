803dc6b6f6cbd88340e1d036730bfc8a85f787fb
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2023-10-27 19:23:24 -0300

    Atividade para primeira inicialização

--- app/src/main/java/com/example/topets/MainActivity.java
+++ app/src/main/java/com/example/topets/MainActivity.java
@@ -9,6 +9,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        //Shows a special greeting screen on first startup. Otherwise shows pet menu screen.
+        int targetActivity = R.layout.activity_main;
+        if(isFirstStart()){
+            //targetActivity = R.layout.<SPECIAL_ACTIVITY_NAME_HERE>
+        }
+
+        setContentView(targetActivity);
+    }
+
+    boolean isFirstStart(){
+        return true;
     }
 }
