3c9fb5cd87e1cfb9019c574e616eeb17eb62b368
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2023-11-09 14:35:33 -0300

    Adicionado lógica da checkbox
    Checkbox agora controla a visibilidade do formulário de lembrete. O estado dessa checkbox deve ser usado para decidir se um lembrete deverá ser criado.

--- app/src/main/java/com/example/topets/AddAppointment.java
+++ app/src/main/java/com/example/topets/AddAppointment.java
@@ -3,12 +3,39 @@ package com.example.topets;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 
 public class AddAppointment extends AppCompatActivity {
+
+    LinearLayout reminderForm;
+    CheckBox reminderCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_appointment);
+
+        reminderForm = findViewById(R.id.reminderForm);
+        reminderCheckBox = findViewById(R.id.checkBox);
+
+        prepareCheckBox();
+    }
+
+    private void prepareCheckBox(){
+        if (this.reminderCheckBox == null || reminderForm == null) {
+            Log.e("Error", "Null reminder check box or reminder form");
+            return;
+        }
+
+        this.reminderCheckBox.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked){
+                this.reminderForm.setVisibility(View.VISIBLE);
+            }else{
+                this.reminderForm.setVisibility(View.INVISIBLE);
+            }
+        });
     }
 }
