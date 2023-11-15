33396e538ccd5a4faa5b120202033b83d693fd16
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2023-11-15 16:56:00 -0300

    Registrar o dispositivo
    Adicionado rotina para registrar o dispositivo no backend assim que o aplicativo inicia.

--- app/src/main/java/com/example/topets/api/services/DeviceService.java
+++ app/src/main/java/com/example/topets/api/services/DeviceService.java
@@ -1,16 +1,17 @@
-package com.example.topets.api;
+package com.example.topets.api.services;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
-import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 
+/**
+ * Provides methods to interact with the devices endpoint of the api.
+ */
 public interface DeviceService{
-    public static final String URL = "api/users";
+    String URL = "v1/device";
 
     @POST(URL+"/{deviceId}")
     Call<ResponseBody> registerDevice(@Path("deviceId") String deviceId);
-
 
 }
