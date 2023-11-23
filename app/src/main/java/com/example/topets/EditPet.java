9398afa27661ae9a29f1ea62db663a0073b1611f
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2023-11-23 13:59:32 -0300

    Implementando datePickerFragment em EditPet
    Mudando implementação do datePicker para um DatePickerFragment.

--- app/src/main/java/com/example/topets/EditPet.java
+++ app/src/main/java/com/example/topets/EditPet.java
@@ -10,6 +10,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.ImageView;
+
+import com.example.topets.fragments.DatePickerFragment;
 import com.google.android.material.textfield.TextInputEditText;
 
 
@@ -22,6 +24,8 @@ public class EditPet extends AppCompatActivity {
      * Activity launcher for getting an image from the user to use as profile picture.
      */
     private ActivityResultLauncher<String> loadImageActivityLauncher;
+
+    DatePickerFragment datePickerFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,8 @@ public class EditPet extends AppCompatActivity {
                     petImage.setImageURI(o);
                 }
         );
+
+        datePickerFragment = new DatePickerFragment(inputData);
 
         setupDateInput();
         setupImageInput();
@@ -63,23 +69,7 @@ public class EditPet extends AppCompatActivity {
 
 
     private void openDatePicker(){
-        final Calendar calendar = Calendar.getInstance();
-
-        DatePickerDialog dialog = new DatePickerDialog(
-                this,
-                new DatePickerDialog.OnDateSetListener(){
-                    @Override
-                    public void onDateSet(DatePicker datePicker, int year, int month, int day){
-                        if(inputData == null ){return;}
-                        inputData.setText(String.valueOf(year)+"."+String.valueOf(month+1)+"."+String.valueOf(day));
-                    }
-                },
-                calendar.get(Calendar.YEAR),
-                calendar.get(Calendar.MONTH),
-                calendar.get(Calendar.DAY_OF_MONTH)
-
-        );
-        dialog.show();
+        this.datePickerFragment.show(getSupportFragmentManager(), "datePicker");
     }
 
 
