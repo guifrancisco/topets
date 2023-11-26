1e42dd60287c3ac8559bd696d7daad38ed511d91
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2023-11-26 17:09:01 -0300

    Implementando serviço

---
+++ app/src/main/java/com/example/topets/api/data/dto/DataReadDiet.java
@@ -0,0 +1,9 @@
+package com.example.topets.api.data.dto;
+
+public class DataReadDiet {
+    String id;
+    String name;
+    String type;
+    String description;
+
+}
---
+++ app/src/main/java/com/example/topets/api/data/dto/DataUpdateDiet.java
@@ -0,0 +1,34 @@
+package com.example.topets.api.data.dto;
+
+import com.example.topets.enums.ActivityType;
+import com.example.topets.enums.RecurrenceType;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.Calendar;
+
+public class DataUpdateDiet {
+    @SerializedName("dataUpdateCommonDetails")
+    DataUpdateActivity dataUpdateActivity;
+    @SerializedName("dataUpdateNutrition")
+    DataUpdateDietDetails dataUpdateDietDetails;
+    DataUpdateReminder dataUpdateReminder;
+
+    public DataUpdateDiet(String name, Boolean deleteReminder, String type, String description, DataUpdateReminder dataUpdateReminder) {
+        this.dataUpdateActivity = new DataUpdateActivity(name, deleteReminder);
+        this.dataUpdateDietDetails = new DataUpdateDietDetails(type, description);
+    }
+
+    public void setReminder(Calendar dateTime, ActivityType activityType, boolean periodic, RecurrenceType recurrenceType, String description){
+        this.dataUpdateReminder = new DataUpdateReminder(dateTime, activityType, periodic, recurrenceType, description);
+    }
+
+    private class DataUpdateDietDetails {
+        String type;
+        String description;
+
+        public DataUpdateDietDetails(String type, String description) {
+            this.type = type;
+            this.description = description;
+        }
+    }
+}
