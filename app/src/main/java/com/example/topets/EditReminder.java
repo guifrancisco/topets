edf056cb514c61afe8cbe14383d60422d23165a3
Igor Rodrigues <77209950+Igor-da-SilvaRodrigues@users.noreply.github.com>
2023-11-27 19:23:09 -0300

    Adicionando telas de menu e edição de lembrete

---
+++ app/src/main/java/com/example/topets/EditReminder.java
@@ -0,0 +1,14 @@
+package com.example.topets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditReminder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_reminder);
+    }
+}
--- app/src/main/java/com/example/topets/Menu.java
+++ app/src/main/java/com/example/topets/Menu.java
@@ -20,6 +20,7 @@ public class Menu extends AppCompatActivity {
     CardView petProfileButton;
     CardView medicationButton;
     CardView dietButton;
+    CardView reminderButton;
     private String petId;
 
     ActivityResultLauncher<Intent> editPetLauncher;
@@ -32,6 +33,7 @@ public class Menu extends AppCompatActivity {
         petProfileButton = findViewById(R.id.petProfileButton);
         medicationButton = findViewById(R.id.medicationButton);
         dietButton = findViewById(R.id.dietButton);
+        reminderButton = findViewById(R.id.reminderButton);
         //registering callback for when the edit pet screen finishes
         editPetLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -48,6 +50,17 @@ public class Menu extends AppCompatActivity {
         preparePetsMenuButton();
         prepareMedicationButton();
         prepareDietButton();
+        prepareReminderButton();
+    }
+
+    private void prepareReminderButton() {
+        reminderButton.setOnClickListener(v -> navigateToReminderMenuScreen());
+    }
+
+    private void navigateToReminderMenuScreen() {
+        Intent intent = new Intent(this, ReminderMenu.class);
+        intent.putExtra("petId", petId);
+        startActivity(intent);
     }
 
     private void prepareDietButton() {
